a94aef9acf30d0f64e1f72f5f68e1b2c997dee50
hwhoj <hwhoj@naver.com>
2025-03-27 22:56:55 +0900

    3. 변수, 상수

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,8 +11,15 @@
 // 네이밍은 숫자로 시작해서는안됨, 특수문자($, _ )는 네이밍 맨앞이나 뒤는 가능하지만 중간에는안된다. 다른특수문자는 그냥 안된다.
 // js는 변수에 값을 할당하여 초기화할 필요는 없다.
 // 문장의 마무리는 세미콜론(;)이 선택사항이다. let const a = 1; let const b = 2와 같이 한줄에 두개의 표현식이 들어가는 경우는 생략불가
-let currentResult = 0;
 
-currentResult = currentResult + 10 * 3;
+// 상수 선언. 변경불가
+const defaultResult = 0;
 
-outputResult(currentResult, '');
+// 변수 선언. 상수를 변수에 할당 가능
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+
+outputResult(currentResult, calculationDescription); 
